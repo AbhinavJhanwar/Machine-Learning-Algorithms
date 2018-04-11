e269cc62d1f957f2f9a3f6575f821b391acb2597
abhinav.jhanwar <abhinav.jhanwar@BDC11-L-7200C5H.dir.svc.accenture.com>
2018-04-11 12:49:24 +0530

    updating wine dataset

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Machine-Learning-Models
 # Guide for python developers to work on machine learning models
-# includes various classification, regression, deep learning & nlp models as seperated by folders
+# includes various classification, regression, deep learning & nlp models
+# usecases included:
+	# wine data
+	# pim_india_diabetes
+	# Machine
+	# ionoshpere
+	# housing
+	# glass
+	# flower petal
+	# cars
+	# cancer
+	# advertising
+	# adult
--- Usecases/wine.py
+++ Usecases/wine.py
@@ -4,13 +4,17 @@ Created on Apr 19, 2017
 @author: abhinav.jhanwar
 '''
 
+#  import pandas library
 import pandas
+# import scatter matrix to plot various scatter plots
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
+# import various sklearn libraries
 from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+# import various models to test
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -18,37 +22,49 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
+# define dataset url
 url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
+# define feature names
 names = ['class','Alcohol','Malic acid','Ash','Alcalinity of ash','Magnesium','Total phenols','Flavanoids','Nonflavanoid phenols',
          'Proanthocyanins','Color intensity','Hue','OD280/OD315 of diluted wines','Proline']
+# read the dataset and convert into pandas dataframe
 dataset = pandas.read_csv(url,names=names)
 
-#print(dataset.shape)
+# check shape of dataset
+print("dataset shape:", dataset.shape)
 
-#print(dataset.head(20))
+# check sample of dataset
+print("\n\nsample dataset:\n", dataset.head(10))
 
-#print(dataset.describe())
+# check details of dataset
+print("\n\ndataset description:\n", dataset.describe())
 
-#print(dataset.groupby('class').size())
+# check various classes counts
+print("\n\ntarget variables counted:\n", dataset.groupby('class').size())
 
-#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-#plt.show()
+# plot scatter plot for all features
+# features can be specifically mentioned to be plotted by passing the required columns
+scatter_matrix(dataset)
+plt.show()
 
-#dataset.hist()
-#plt.show()
-
-#scatter_matrix(dataset)
-#plt.show()
-
+# extract only values from dataset
 array = dataset.values
+
+# define X
 X = array[:,1:15]
+
+# define Y
 Y = array[:,0]
 validation_size = 0.20
 seed = 7
+
+# split the data in training and validation sets
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
+# define scoring criteria
 scoring = 'accuracy'
 
+# define various models to be tested
 models = []
 models.append(('LR', LogisticRegression()))
 models.append(('LDA', LinearDiscriminantAnalysis()))
@@ -56,6 +72,7 @@ models.append(('KNN', KNeighborsClassifier()))
 models.append(('CART', DecisionTreeClassifier()))
 models.append(('NB', GaussianNB()))
 models.append(('SVM', SVC()))
+
 # evaluate each model in turn
 results = []
 names = []
@@ -67,14 +84,15 @@ for name, model in models:
     msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
     print(msg)
     
-# Compare Algorithms
-#fig = plt.figure()
-#fig.suptitle('Algorithm Comparison')
-#ax = fig.add_subplot(111)
-#plt.boxplot(results)
-#ax.set_xticklabels(names)
-#plt.show()
+# Compare Algorithms from their cross validation scores
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
 
+# take best model and predict data
 model = LinearDiscriminantAnalysis()
 model.fit(X_train, Y_train)
 predictions = model.predict(X_validation)
